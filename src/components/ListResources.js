ee3450d298d90654e9cbef7533277a38a33999d9
Benjamin Lavena <benja@zmaster.com.ar>
2020-10-13 20:42:28 -0300

    arreglado el tema de los keys en los map

--- src/components/ListResources.js
+++ src/components/ListResources.js
@@ -1,21 +1,20 @@
 import React from "react";
+import { v4 as uuidv4 } from "uuid";
+
 export default function ListResources(props) {
   if (props.loading) {
-    return <div>Loading...</div>;
+    return <div className="text-white text-4xl">Loading...</div>;
   }
   return (
     <div>
       <hr className="border-t-2 border-blue-800 mt-6" />
-      <h1 className="text-4xl">{props.type}</h1>
+      <h1 className="text-4xl text-white">{props.type}</h1>
       {props.data.map((item) => (
-        <ul>
-          <li
-            key={item.id}
-            className="bg-white my-6 rounded-lg shadow-lg py-6 px-4"
-          >
-            {Object.keys(item).map((property, key) => (
-              <ul>
-                <li key={key}>
+        <ul key={uuidv4()}>
+          <li className="bg-white my-6 rounded-lg shadow-lg py-6 px-4">
+            {Object.keys(item).map((property) => (
+              <ul key={uuidv4()}>
+                <li>
                   <strong>{property}</strong>: {JSON.stringify(item[property])}
                 </li>
               </ul>
